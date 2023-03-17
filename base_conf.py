05500bbf022ab86dd3feaf09f1791d43c2af3166
austin01.xu <austin01.xu@vipshop.com>
2023-03-17 21:03:37 +0800

    update conf

--- base_conf.py
+++ base_conf.py
@@ -2,26 +2,28 @@
 # by allen.xu
 # 解析配置文件的库
 
+import os
+import copy
 import json
 import yaml
 
-class Conf:
+class BaseConf:
     def __init__(self) -> None:
         current_dir = os.path.dirname(os.path.abspath(__file__))
         os.chdir(current_dir)
 
-        with open('./conf/mercenary.yml') as f:
-            self.mer_conf = yaml.safe_load(f)
-        with open('./conf/instance.yml') as f:
-            self.instance_conf = yaml.safe_load(f)
+        with open('./base_conf.yml') as f:
+            self.base_conf = yaml.safe_load(f)
     
     def get_mer_team_conf(self,mer_name):
-        return self.mer_conf['佣兵'][mer_name]
+        return copy.deepcopy(self.base_conf['佣兵'][mer_name])
     
+    def get_mer_names_by_team_name(self, team_name):
+        return copy.deepcopy(self.base_conf['队伍'][team_name])
     def print_mer(self):
-        print(type(self.mer_conf['佣兵']['拉格纳罗斯']['碎片来源']))
-        print(len(self.mer_conf['佣兵']['拉格纳罗斯']['碎片来源']))
-        print(self.mer_conf['佣兵']['拉格纳罗斯']['碎片来源'])
+        print(type(self.base_conf['佣兵']['拉格纳罗斯']['碎片来源']))
+        print(len(self.base_conf['佣兵']['拉格纳罗斯']['碎片来源']))
+        print(self.base_conf['佣兵']['拉格纳罗斯']['碎片来源'])
     
 #获取佣兵在队伍里面的配置
 def get_mer_team_conf(mer_name):
@@ -35,7 +37,7 @@ if __name__ == '__main__':
     # 打印当前工作目录
     print(os.getcwd())
 
-    base_conf = Conf()
+    base_conf = BaseConf()
     base_conf.print_mer()
     print(base_conf.get_mer_team_conf('拉格纳罗斯'))
     
